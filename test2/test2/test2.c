db1b3cb55fee16293fbfda1527ca96b41d86a80b
chen-songyun0129 <2642906810@qq.com>
2022-11-12 21:38:31 +0800

    实验室考核一

--- test2/test2/test2.c
+++ test2/test2/test2.c
@@ -7,24 +7,21 @@ int main()
 	int sums[100];
 	int cnt = 0;
 	int i = 0;
+	int j = 0;
 	printf("请输入所求数组\n");
 	scanf("%d", &x);//初始化
 
 	while (x != -1) {
 		sum[cnt] = x;
+		sums[j] = x;
 		cnt++;
+		j++;
 		scanf("%d", &x);
 	}//为数组中每个值赋值
 
 	int targe;
 	printf("请输入所求值\n");
 	scanf("%d", &targe);//输入所求值
-
-	int j = 0;
-	for (i = 0, j = 0; i < sizeof(sum) / sizeof(sum[0]), j < sizeof(sum) / sizeof(sum[0]); j++, i++)
-	{
-		sums[j] = sum[i];
-	}//将sums的每个值赋给一个新的sum的函数
 
 	for (i = 0; i < sizeof(sum) / sizeof(sum[0]); i++) {
 		for (j = 0; j < sizeof(sum) / sizeof(sum[0]); j++) {
